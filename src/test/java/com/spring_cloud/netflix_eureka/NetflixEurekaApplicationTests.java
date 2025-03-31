04b3c8407f070d1caf84315d727056c3823f228e
praneshmeher <praneshmeher@gmail.com>
2025-03-31 20:24:00 +0530

    service registry

---
+++ src/test/java/com/spring_cloud/netflix_eureka/NetflixEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.netflix_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
